1e8eecfe72f621ba404c2d125e33b1916e0b69b6
samiryousri <samiryousri96@gmail.com>
2024-05-25 23:09:22 +0300

    add hive and  generate type adapter and local data source

---
+++ lib/Features/home/data/data_source/local_data_source.dart
@@ -0,0 +1,20 @@
+import 'package:bookly/Features/home/domain/entities/book_entity.dart';
+
+abstract class HomeLocalDataSource {
+  List<BookEntity> fetchFeaturedBooks({int pageNumber = 0});
+  List<BookEntity> fetchNewestBooks();
+}
+
+class HomeLocalDataSourceImpl extends HomeLocalDataSource {
+  @override
+  List<BookEntity> fetchFeaturedBooks({int pageNumber = 0}) {
+    // TODO: implement fetchFeaturedBooks
+    throw UnimplementedError();
+  }
+
+  @override
+  List<BookEntity> fetchNewestBooks() {
+    // TODO: implement fetchNewestBooks
+    throw UnimplementedError();
+  }
+}
