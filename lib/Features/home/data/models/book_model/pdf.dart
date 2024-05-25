2477952b1c6dcfbf20f5b1158085d2fc6a8817c9
samiryousri <samiryousri96@gmail.com>
2024-05-25 14:51:22 +0300

    add book model

---
+++ lib/Features/home/data/models/book_model/pdf.dart
@@ -0,0 +1,13 @@
+class Pdf {
+  final bool? isAvailable;
+
+  const Pdf({this.isAvailable});
+
+  factory Pdf.fromJson(Map<String, dynamic> json) => Pdf(
+        isAvailable: json['isAvailable'] as bool?,
+      );
+
+  Map<String, dynamic> toJson() => {
+        'isAvailable': isAvailable,
+      };
+}
